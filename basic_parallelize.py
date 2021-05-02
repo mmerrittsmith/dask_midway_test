3d389c2992d4dcec80e5aec0a66f46656bc6c982
mmerrittsmith <matthewmerrittsmith@gmail.com>
2021-05-01 20:58:07 -0500

    adding basic_parallelize to get DeS summaries

---
+++ basic_parallelize.py
@@ -0,0 +1,48 @@
+# basic_parallelize.py
+
+def main():
+	filenames = grab_DeS_GADM_codes()
+	sbatch_scripts = [gen_sbatch_script(code_set, file_num) for file_num, code_set in enumerate(filenames)]
+	for script_num, sbatch_script in enumerate(sbatch_scripts):
+		with open('sbatch_holder/'+str(script_num)+'.sbatch', 'w+') as f:
+			f.write(sbatch_script)
+		subprocess.run(['sbatch', 'sbatch_holder/'+str(script_num)+'.sbatch'])
+
+def grab_DeS_GADM_codes():
+    with open('DeS_GADM_codes.txt', 'r+') as read_file:
+        DeS_codes = read_file.read()
+    DeS_codes = DeS_codes.split(', ')
+    filenames = [filename_helper(code) for code in DeS_codes]
+    return filenames
+
+
+def filename_helper(code):
+    block_path = '/project2/bettencourt/mnp/prclz/data/blocks/Africa/TZA/blocks_'+code+'.csv'
+    summary_path = '/home/merrittsmith/block_summaries/TZA/'+code+'.geojson'
+    return {'block path': block_path, 'summary_path': summary_path}
+
+
+def gen_sbatch_script(code_set, job_num):
+    # long-term I think it makes sense to integrate this dask stuff with block_summary 
+    # but for now we'll access it via the CLI
+    last_line = ' '.join(['python', '/home/merrittsmith/mnp-analysis/utils/block_summary.py',
+                    '--aoi_path', code_set['block path'],
+                    '--landscan_path', '/project2/bettencourt/mnp/prclz/data/LandScan_Global_2018/raw_tif/ls_2018.tif',
+                    '--buildings_dir', '/project2/bettencourt/mnp/prclz/data/buildings/Africa/TZA',
+                    '--blocks_dir', '/project2/bettencourt/mnp/prclz/data/blocks/Africa/TZA',
+                    '--gadm_dir', '/project2/bettencourt/mnp/prclz/data/GADM/TZA',
+                    '--summary_out_path', code_set['summary_path']])
+    sbatch_options = '\n'.join(['--job-name=parallel_block_summary_test_'+str(job_num), '--output=DeS_parallel_summary.out', 'error=DeS_parallel_block_summary.err',
+                      '--time=8:00:00', '--partition=broadwl', '--nodes=1', '--ntasks-per-node=1', '--mem-per-cpu=40000', 
+                      '--mail-type=END', '--mail-user=merrittsmith@uchicago.edu'])
+    sbatch_script = '\n'.join(['#!/bin/bash', sbatch_options, last_line])
+
+    return sbatch_script
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
--- cluster_compute_DeS.py
+++ cluster_compute_DeS.py
@@ -1,52 +1,59 @@
-from dask_jobqueue import SLURMCluster
-from dask.distributed import Client
+# from dask_jobqueue import SLURMCluster
+from dask.distributed import Client, performance_report, LocalCluster
 import subprocess
 import logging
 import bokeh
 
 
 def main():
-	logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-	filenames = grab_DeS_GADM_codes()
-	cluster = SLURMCluster(cores=24,
-					   memory='40GB',
-					   walltime='08:00:00',
-					   log_directory='/home/merrittsmith/dask_log',
-					   local_directory='/home/merrittsmith/dask_out',
-					   job_extra=['--partition=broadwl'])
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    filenames = grab_DeS_GADM_codes()
+    # c = LocalCluster()
+    # client = Client(c)
+    # c.start_diagnostics_server()
+    # client.submit(run_block_summary, filenames)
 
-	cluster.adapt(maximum_jobs=20)
-	client = Client(cluster)
-	with performance_report(filename='DeS_dask_report.html'):
-		client.map(run_block_summary, filenames)
-	print(cluster.job_script())
+    cluster = SLURMCluster(cores=24,
+                           memory='40GB',
+                           walltime='08:00:00',
+                           log_directory='/home/merrittsmith/dask_log',
+                           local_directory='/home/merrittsmith/dask_out',
+                           job_extra=['--partition=broadwl'],
+                           protocol='ib0')
+
+    cluster.scale(8)
+    client = Client(cluster)
+
+    with performance_report(filename='DeS_dask_repotr.html'):
+       client.submit(run_block_summary, filenames)
+    print(cluster.job_script())
 
 def grab_DeS_GADM_codes():
-	with open('DeS_GADM_codes.txt', 'r+') as read_file:
-		DeS_codes = read_file.read()
-	DeS_codes = DeS_codes.split(', ')
-	filenames = [filename_helper(code) for code in DeS_codes]
-	return filenames
+    with open('DeS_GADM_codes.txt', 'r+') as read_file:
+        DeS_codes = read_file.read()
+    DeS_codes = DeS_codes.split(', ')
+    filenames = [filename_helper(code) for code in DeS_codes]
+    return filenames
 
 
 def filename_helper(code):
-	block_path = '/project2/bettencourt/mnp/prclz/data/blocks/Africa/TZA/blocks_'+code+'.csv'
-	summary_path = '/home/merrittsmith/block_summaries/TZA/'+code+'.geojson'
-	return {'block path': block_path, 'summary_path': summary_path}
+    block_path = '/project2/bettencourt/mnp/prclz/data/blocks/Africa/TZA/blocks_'+code+'.csv'
+    summary_path = '/home/merrittsmith/block_summaries/TZA/'+code+'.geojson'
+    return {'block path': block_path, 'summary_path': summary_path}
 
 
 def run_block_summary(code_set):
-	# long-term I think it makes sense to integrate this dask stuff with block_summary 
-	# but for now we'll access it via the CLI
-	subprocess.run(['python', '/home/merrittsmith/mnp-analysis/utils/block_summary.py',
-	                '--aoi_path', code_set['block path'],
-	                '--landscan_path', '/project2/bettencourt/mnp/prclz/data/LandScan_Global_2018/raw_tif/ls_2018.tif',
-	                '--buildings_dir', '/project2/bettencourt/mnp/prclz/data/buildings/Africa/TZA',
-	                '--blocks_dir', '/project2/bettencourt/mnp/prclz/data/blocks/Africa/TZA',
-	                '--gadm_dir', '/project2/bettencourt/mnp/prclz/data/GADM/TZA',
-	                '--summary_out_path', code_set['summary_path']],
-	                check=True,
-	                cwd='/home/merrittsmith')
+    # long-term I think it makes sense to integrate this dask stuff with block_summary 
+    # but for now we'll access it via the CLI
+    subprocess.run(['python', '/home/merrittsmith/mnp-analysis/utils/block_summary.py',
+                    '--aoi_path', code_set['block path'],
+                    '--landscan_path', '/project2/bettencourt/mnp/prclz/data/LandScan_Global_2018/raw_tif/ls_2018.tif',
+                    '--buildings_dir', '/project2/bettencourt/mnp/prclz/data/buildings/Africa/TZA',
+                    '--blocks_dir', '/project2/bettencourt/mnp/prclz/data/blocks/Africa/TZA',
+                    '--gadm_dir', '/project2/bettencourt/mnp/prclz/data/GADM/TZA',
+                    '--summary_out_path', code_set['summary_path']],
+                    check=True,
+                    cwd='/home/merrittsmith')
 
 if __name__ == '__main__':
-	main()
+    main()
